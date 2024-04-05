eae4209568573f32818c18d4b8b3994e81decdcd
Karina Rozenberga Korneva <karinilya@mail.ru>
2024-04-05 18:09:53 +0200

    Модификация функций, добавление функции для получения постов пользователя

--- api.js
+++ api.js
@@ -1,31 +1,42 @@
 // Замени на свой, чтобы получить независимый от других набор данных.
 // "боевая" версия инстапро лежит в ключе prod
-const personalKey = "prod";
+const personalKey = "karina-roz";
 const baseHost = "https://webdev-hw-api.vercel.app";
 const postsHost = `${baseHost}/api/v1/${personalKey}/instapro`;
+const userHost = `${baseHost}/api/v1/${personalKey}/instapro/user-posts`;
 
-export function getPosts({ token }) {
-  return fetch(postsHost, {
+export async function getPosts({ token }) {
+  const response = await fetch(postsHost, {
     method: "GET",
     headers: {
       Authorization: token,
     },
   })
-    .then((response) => {
-      if (response.status === 401) {
+   if (response.status === 401) {
         throw new Error("Нет авторизации");
       }
+      const data = await response.json();
+      return data.posts;  
+}
 
-      return response.json();
-    })
-    .then((data) => {
-      return data.posts;
-    });
+export async function getUserPosts({ id }) {
+  console.log(id)
+  const response = await fetch(`${userHost}/${id}`, {
+    method: "GET",
+    headers: {
+      Authorization: getToken(),
+    },
+  });
+  if (response.status === 401) {
+    throw new Error("Нет авторизации");
+  }
+  const data_1 = await response.json();
+  return data_1.posts;
 }
 
 // https://github.com/GlebkaF/webdev-hw-api/blob/main/pages/api/user/README.md#%D0%B0%D0%B2%D1%82%D0%BE%D1%80%D0%B8%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D1%8C%D1%81%D1%8F
-export function registerUser({ login, password, name, imageUrl }) {
-  return fetch(baseHost + "/api/user", {
+export async function registerUser({ login, password, name, imageUrl }) {
+  const response = await fetch(userHost + "/api/user", {
     method: "POST",
     body: JSON.stringify({
       login,
@@ -33,38 +44,35 @@ export function registerUser({ login, password, name, imageUrl }) {
       name,
       imageUrl,
     }),
-  }).then((response) => {
+  });
     if (response.status === 400) {
       throw new Error("Такой пользователь уже существует");
     }
-    return response.json();
-  });
+    return await response.json();  
 }
 
-export function loginUser({ login, password }) {
-  return fetch(baseHost + "/api/user/login", {
+export async function loginUser({ login, password }) {
+  const response = await fetch(`${userHost}/login`, {
     method: "POST",
     body: JSON.stringify({
       login,
       password,
     }),
-  }).then((response) => {
+  });
     if (response.status === 400) {
       throw new Error("Неверный логин или пароль");
     }
-    return response.json();
-  });
+    return await response.json(); 
 }
 
 // Загружает картинку в облако, возвращает url загруженной картинки
-export function uploadImage({ file }) {
+export async function uploadImage({ file }) {
   const data = new FormData();
   data.append("file", file);
 
-  return fetch(baseHost + "/api/upload/image", {
+  const response = await fetch(`${baseHost}/api/upload/image`, {
     method: "POST",
     body: data,
-  }).then((response) => {
-    return response.json();
   });
+    return await response.json(); 
 }
