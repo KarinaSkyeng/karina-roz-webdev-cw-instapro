60b87d26c2073f74c53ae786f8959e7eec5b2e4e
Karina Rozenberga Korneva <karinilya@mail.ru>
2024-05-12 21:11:58 +0200

    создание функций лайка и дислайка

--- api.js
+++ api.js
@@ -1,75 +1,61 @@
+import { getToken } from "./index.js";
+import { getUserFromLocalStorage } from "./helpers.js";
+
 // Замени на свой, чтобы получить независимый от других набор данных.
 // "боевая" версия инстапро лежит в ключе prod
 const personalKey = "karina-roz";
 const baseHost = "https://webdev-hw-api.vercel.app";
 const postsHost = `${baseHost}/api/v1/${personalKey}/instapro`;
-const userHost = `${baseHost}/api/v1/${personalKey}/instapro/user-posts`;
 
-export async function getPosts({ token }) {
-  const response = await fetch(postsHost, {
+export function getPosts({ token }) {
+  return fetch(postsHost, {
     method: "GET",
     headers: {
       Authorization: token,
     },
-  });
+  })
+    .then((response) => {
+      if (response.status === 401) {
+        throw new Error("Нет авторизации");
+      }
 
-  if (response.status === 401) {
-    throw new Error("Нет авторизации");
-  }
-
-  const data = await response.json();
-  return data.posts;  
+      return response.json();
+    })
+    .then((data) => {
+      return data.posts;
+    });
 }
 
-export async function getUserPosts( id, { token }) {
-  console.log(id)
-  const response = await fetch(`${userHost}/${id}`, {
+export function getUserPosts({id}) {
+  console.log(id);
+  return fetch(postsHost + `/user-posts/${id}`, {
     method: "GET",
-    headers: {
-      Authorization: token,
-    },
+  }).then((response) => {
+    return response.json();
+  }).then((data) => {
+    return data.posts;
   });
-
-  if (response.status === 401) {
-    throw new Error("Нет авторизации");
-  }
-  
-  if (!response.ok) {
-    throw new Error("Ошибка при получении данных пользователя");
-  }
-
-  const userData = await response.json();
-  return userData.posts;
 }
 
-export async function postPost({description, imageUrl}) {
-  try {
-  const response = await fetch(postsHost, {
-    method: "POST",
-    body: JSON.stringify({
-         description,
-         imageUrl,
-    }),
+export function addPost({ description, imageUrl }) {
+  return fetch(postsHost, {
+    method: 'POST',
     headers: {
       Authorization: getToken(),
     },
-  });
-
-      if (response.status === 201) {
-        return response.json();
-      } else if (response.status === 400) {
-        alert("Введите описание картинки и/или добавьте ссылку на фото");
-          throw new Error("Введите описание картинки и/или добавьте ссылку на фото");
-      }
-    } catch (error) {
-      console.error("Ошибка при создании поста:", error);
-      throw error;
-    } 
+    body: JSON.stringify({
+      description,
+      imageUrl,
+    }),
+  })
+  .then((response) => {
+    return response.json();
+  })
 }
 
 // https://github.com/GlebkaF/webdev-hw-api/blob/main/pages/api/user/README.md#%D0%B0%D0%B2%D1%82%D0%BE%D1%80%D0%B8%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D1%8C%D1%81%D1%8F
-export async function registerUser({ login, password, name, imageUrl }) {
-  const response = await fetch(userHost + "/api/user", {
+export function registerUser({ login, password, name, imageUrl }) {
+  return fetch(baseHost + "/api/user", {
     method: "POST",
     body: JSON.stringify({
       login,
@@ -77,65 +63,98 @@ export async function registerUser({ login, password, name, imageUrl }) {
       name,
       imageUrl,
     }),
-  });
+  }).then((response) => {
     if (response.status === 400) {
       throw new Error("Такой пользователь уже существует");
     }
-    return response.json();  
+    return response.json();
+  });
 }
 
-export async function loginUser({ login, password }) {
-  const response = await fetch(`${userHost}/login`, {
+export function loginUser({ login, password }) {
+  return fetch(baseHost + "/api/user/login", {
     method: "POST",
     body: JSON.stringify({
       login,
       password,
     }),
-  });
+  }).then((response) => {
     if (response.status === 400) {
       throw new Error("Неверный логин или пароль");
     }
-    return await response.json(); 
+    return response.json();
+  });
 }
 
 // Загружает картинку в облако, возвращает url загруженной картинки
-export async function uploadImage({ file }) {
+export function uploadImage({ file }) {
   const data = new FormData();
   data.append("file", file);
 
-  const response = await fetch(`${baseHost}/api/upload/image`, {
+  return fetch(baseHost + "/api/upload/image", {
     method: "POST",
     body: data,
+  }).then((response) => {
+    return response.json();
   });
-    return await response.json(); 
 }
 
-export async function likePost(id, { token }) {
-  const response = await fetch(`${postsHost}/${id}/like`, {
+export const getLike = (id, { token }) => {
+  return fetch(`${postsHost}/${id}/like`, {
     method: "POST",
     headers: {
       Authorization: token,
-    },
-  });
-
-  if (response.status === 200) {
+    }
+  })
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`Ошибка ${response.status}: ${response.statusText}`);
+    }
     return response.json();
-  } else {
-    throw new Error("Лайкать посты могут только авторизованные пользователи");
-  }
-}
+  })
+    .catch((error) => {
+      alert('Вы не авторизованы!')
+      throw error;
+    });
 
-export async function dislikeLike(id, { token }) {
-  const response = await fetch(`${postsHost}/${id}/dislike`, {
+};
+
+export const getDislike = (id, { token }) => {
+  return fetch(`${postsHost}/${id}/dislike`, {
     method: "POST",
     headers: {
       Authorization: token,
-    },
-  });
+    }
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Ошибка ${response.status}: ${response.statusText}`);
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      console.error("Ошибка при удалении лайка:", error);
+      alert('Что-то пошло не так, попробуйте позже.')
+      throw error;
+    });
+};
 
-  if (response.status === 200) {
-    return response.json();
-  } else {
-    throw new Error("Лайкать посты могут только авторизованные пользователи");
-  }
+export function getPostsWithToken() {
+  const token = getToken();
+  return fetch(postsHost, {
+    method: "GET",
+    headers: {
+      Authorization: token,
+    },
+  })
+    .then((response) => {
+      if (response.status === 401) {
+        throw new Error("Нет авторизации");
+      }
+
+      return response.json();
+    })
+    .then((data) => {
+      return data.posts;
+    });
 }
--- components/add-post-page-component.js
+++ components/add-post-page-component.js
@@ -1,31 +1,62 @@
-export function renderAddPostPageComponent({ appEl, onAddPostClick }) {
+import { getToken,goToPage } from "../index.js";
+import { renderHeaderComponent } from "./header-component.js";
+import { renderUploadImageComponent } from "./upload-image-component.js";
+import { addPost } from "../api.js";
+import { POSTS_PAGE } from "../routes.js";
+
+export function renderAddPostPageComponent({ appEl }) {
+  let imageUrl = '';
+
   const render = () => {
-    // TODO: Реализовать страницу добавления поста
     const appHtml = `
     <div class="page-container">
       <div class="header-container"></div>
-      <input type="text" id="description-input" placeholder="Введите описание">
-      <input type="text" id="image-url-input" placeholder="Введите URL изображения">
-      <button class="button" id="add-button">Добавить</button>
-    </div>
-  `;
+      <div class="form">
+        <h3 class="form-title">Добавить пост</h3>
+        <div class="form-inputs">
+          <div class="upload-image-container"></div>
+          <label>Описание:</label>
+          <textarea class="input textarea" id='textarea-input' rows="4"></textarea>
+          <button class="button" id="add-button">Добавить</button>
+        </div>
+      </div>
+    </div>`;
 
     appEl.innerHTML = appHtml;
 
+    renderHeaderComponent({
+      element: document.querySelector(".header-container"),
+    });
+
+    renderUploadImageComponent({
+      element: appEl.querySelector(".upload-image-container"),
+      onImageUrlChange(newImageUrl) {
+        imageUrl = newImageUrl;
+      },
+    });
+
     document.getElementById("add-button").addEventListener("click", () => {
-      const description = document.getElementById("description-input").value;
-      const imageUrl = document.getElementById("image-url-input").value;
-    
-      // Проверка на пустые поля
-      if (description.trim() === '' || imageUrl.trim() === '') {
-        alert('Пожалуйста, заполните все поля');
-        return;
+      const description = document.getElementById('textarea-input');
+      if (description.value === "") {
+        alert('Заполните поле');
+      } else if (!imageUrl) {
+        alert('Добавьте фотографию');
+      } else {
+        addPostToAPI(description.value, imageUrl);
       }
-    
-      // Вызов функции обработчика с введенными данными
-      onAddPostClick({ description, imageUrl });
-    }); 
-  }
+    });
+  };
+
+  const addPostToAPI = (description, imageUrl) => {
+    addPost({ description, imageUrl, token: getToken() })
+      .then(() => {
+        goToPage(POSTS_PAGE);
+      })
+      .catch((error) => {
+        console.error("Ошибка при добавлении поста:", error);
+        alert("Произошла ошибка при добавлении поста. ");
+      });
+  };
 
   render();
 }
